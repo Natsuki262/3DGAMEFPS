acc265ae11d4643ca313fd14cb64dc6d1bd6d129
NatsukiAlen <hiro.58.fgl.2525.9552@icloud.com>
2021-02-11 20:58:47 +0900

    仮ゴールエリアに接触すると、ゴールシーンに遷移

--- FOSZONBIGAME/Assets/zisaku/Goal.cs
+++ FOSZONBIGAME/Assets/zisaku/Goal.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Goal : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -14,5 +14,9 @@ public class Goal : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene("clear");
     }
 }
