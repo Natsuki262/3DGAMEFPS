c78892e70675e29fdf1fcca63ee471b25f162ba9
NatsukiAlen <hiro.58.fgl.2525.9552@icloud.com>
2021-02-14 18:19:20 +0900

    ボタンを押したらドアが開くアニメーションの後ゲームシーンに移行することに成功

--- FOSZONBIGAME/Assets/zisaku/DorOpen.cs
+++ FOSZONBIGAME/Assets/zisaku/DorOpen.cs
@@ -6,13 +6,25 @@ public class DorOpen : MonoBehaviour
 {
     [SerializeField]
     Animator animator;
-   
+
     // Start is called before the first frame update
-    
+    private void Start()
+    {
+
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.Play("DorOpen", 0, 0f);
+        }
+
+    }
 
     public void Open()
     {
 
-        animator.Play("DorOpen", 0, 1f);
+        Debug.Log("再生開始");
+        animator.Play("DorOpen", 0, 0f);
     }
 }
