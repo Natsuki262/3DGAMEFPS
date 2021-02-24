adb484e8a8eab7eee3ca37c5fa130b129a6adb2a
NatsukiAlen <hiro.58.fgl.2525.9552@icloud.com>
2021-02-25 00:29:24 +0900

    音ファイル追加エネミー追尾プログラム作成中

--- FOSZONBIGAME/Assets/zisaku/Playersearch.cs
+++ FOSZONBIGAME/Assets/zisaku/Playersearch.cs
@@ -4,19 +4,43 @@ using UnityEngine;
 using UnityEngine.AI;
 public class Playersearch : MonoBehaviour
 {
+    public Transform targetObject;
     // Start is called before the first frame update
+    /// <summary>
+    /// 目標到達オブジェクト
+    /// </summary>
+    public GameObject goal;
+    public NavMeshAgent agent;
+    /// <summary>
+    /// 距離
+    /// </summary>
+    public float distance;
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        goal = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("検出");
+        Debug.Log("プレイヤー");
+
+        if (other.tag == "Player")
+        {
+            Debug.Log("まいあひー");
+            transform.LookAt(targetObject);
+            distance = Vector3.Distance(transform.position, goal.transform.position);
+            Debug.Log(distance);
+            if (distance < 1)
+            {
+                agent.destination = goal.transform.position;
+            }
+        }
+
     }
 }
